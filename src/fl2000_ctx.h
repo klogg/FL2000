4227d7130e7c7fd121905c43d7dcd151f7b7816d
Hans Ulli Kroll <ulli.kroll@googlemail.com>
2017-12-09 19:59:53 +0100

    remove unused v_sync_time, v_back_porch
    Signed-off-by: Hans Ulli Kroll <ulli.kroll@googlemail.com>

--- src/fl2000_ctx.h
+++ src/fl2000_ctx.h
@@ -30,8 +30,6 @@ struct fl2000_timing_entry {
 	uint32_t 	h_sync_time;
 	uint32_t 	h_back_porch;
 	uint32_t 	v_total_time;
-	uint32_t 	v_sync_time;
-	uint32_t 	v_back_porch;
 	uint32_t 	h_sync_reg_1;
 	uint32_t 	h_sync_reg_2;
 	uint32_t 	v_sync_reg_1;
